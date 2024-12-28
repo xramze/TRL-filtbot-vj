ec45163ddddb90308e45468f29d29f54e7ad33b8
Ram <166788107+xramze@users.noreply.github.com>
2024-12-28 22:58:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 
 ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ɢɪᴠᴇ ᴍᴏᴠɪᴇs ᴏʀ sᴇʀɪᴇs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘᴍ!
 
-<blockquote> 〽️ Powered by <a href=t.me/itsz_ram>𝐈𝛕ᷟ‌𝚣ꙴ 𑐊‌𑂞ဖᮀ™</a></a></blockquote></b>"""
+<blockquote> 〽️ Powered by - <a href=t.me/itsz_ram>𝐈𝛕ᷟ‌𝚣ꙴ 𑐊‌𑂞ဖᮀ™</a></a></blockquote></b>"""
 
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
     
