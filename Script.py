6779aa74f90b5673af4ca5cf834c4ce5c79f0a65
Ram <166788107+xramze@users.noreply.github.com>
2024-12-28 21:15:14 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,9 +3,11 @@
 # Ask Doubt on telegram @KingVJ01
 
 class script(object):
-    START_TXT = """<b><blockquote>☺️ ʜᴇʏ ᴅᴜᴅᴇ {}♥️, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
+    START_TXT = """<b><blockquote>☺️ ʜᴇʏ ᴅᴜᴅᴇ {}♥️ 
+    ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
 
-ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏꜰɪʟᴛᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ɢɪᴠᴇ ᴍᴏᴠɪᴇs ᴏʀ sᴇʀɪᴇs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘᴍ !! ⚡ <blockquote> 〽️ Powered by <a href=t.me/itsz_ram>𝐈𝛕ᷟ‌𝚣ꙴ 𑐊‌𑂞ဖᮀ™</a></a></blockquote></b>"""
+ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏꜰɪʟᴛᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ɢɪᴠᴇ ᴍᴏᴠɪᴇs ᴏʀ sᴇʀɪᴇs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘᴍ !! ⚡
+<blockquote> 〽️ Powered by <a href=t.me/itsz_ram>𝐈𝛕ᷟ‌𝚣ꙴ 𑐊‌𑂞ဖᮀ™</a></a></blockquote></b>"""
 
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
     
