1acce7fa62bc5274d98991ef97e1844805506fd8
Ram <166788107+xramze@users.noreply.github.com>
2025-03-04 15:26:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,8 +126,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '8ac39c0077de07d53e311541b6a9a85b9eb2cb18')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'papajiurl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '331509401d5ae5ac1f10523729138ff2bc6c76e4')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/TamilRockers_Tutorial/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
